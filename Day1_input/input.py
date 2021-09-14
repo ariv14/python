97faa48159c880418fa302cc20dc5e56cfa733a5
Arivoli Murugan <arimatch1@gmail.com>
2021-09-15 00:23:39 +0800

    input updated

--- Day1_input/input.py
+++ Day1_input/input.py
@@ -1,1 +1,5 @@
-3 * (3 + 3) / 3 - 3
+# With python v 2 use raw_input() function
+print("Hello " + raw_input("What is your name?"))
+
+# With python v3 use input() function
+print("Hello " + input("What is your name?"))
