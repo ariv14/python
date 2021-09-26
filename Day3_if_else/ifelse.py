ac48d2d7f2d191d401edd4f1d6397a14f6c44e26
Arivoli Murugan <arimatch1@gmail.com>
2021-09-26 18:13:05 +0800

    Conditions if_else

---
+++ Day3_if_else/ifelse.py
@@ -0,0 +1,11 @@
+# Even or odd number finder
+print("Welcome!! Please input any number to check odd or even !!\n")
+# Get the input number as integer
+number = int(input("Which number do you want to check? "))
+
+#Write your code below this line
+
+if number % 2 == 0:
+    print("The number is even number")
+else:
+    print("The number is odd number")
